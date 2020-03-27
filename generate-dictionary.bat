5265e4e4ecb2df18af86fa3ac4e626468cd9e51a
AioiLight <info@aioilight.space>
2020-03-27 18:11:14 +0900

    辞書生成スクリプトを更新

--- generate-dictionary.bat
+++ generate-dictionary.bat
@@ -12,9 +12,14 @@ exit
 
 :generate
 
+rem キャラクター
 call "tool\DictionaryMate" "--input" "data\chara.json" "--output" "%1%2" "--ime" "%2"
 move "%1%2\%2.txt" "%1%2\chara.txt"
+rem ギルド名
 call "tool\DictionaryMate" "--input" "data\guild.json" "--output" "%1%2" "--ime" "%2"
 move "%1%2\%2.txt" "%1%2\guild.txt"
+rem 曲
+call "tool\DictionaryMate" "--input" "data\song.json" "--output" "%1%2" "--ime" "%2"
+move "%1%2\%2.txt" "%1%2\song.txt"
 
 exit /b
