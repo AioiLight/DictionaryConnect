b47366832cf1d7af87383214f8eb23e18ddeeb5b
MISONLN41 <misonln41@protonmail.com>
2021-01-10 19:45:30 +0900

    generate-dictionary.bat のプレイアブルキャラクターの JSON ファイルのパスを修正

--- generate-dictionary.bat
+++ generate-dictionary.bat
@@ -13,8 +13,8 @@ exit
 :generate
 
 rem キャラクター
-call "tool\DictionaryMate" "--input" "data\chara.json" "--output" "%1%2" "--ime" "%2"
-move "%1%2\%2.txt" "%1%2\chara.txt"
+call "tool\DictionaryMate" "--input" "data\playable.json" "--output" "%1%2" "--ime" "%2"
+move "%1%2\%2.txt" "%1%2\playable.txt"
 rem ギルド名
 call "tool\DictionaryMate" "--input" "data\guild.json" "--output" "%1%2" "--ime" "%2"
 move "%1%2\%2.txt" "%1%2\guild.txt"
